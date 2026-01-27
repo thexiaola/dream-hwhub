115ff61fada7ec63bf11c379b6f03905baa9b1e7
TheXiaoLa <167291828@qq.com>
2026-01-27 17:11:33 +0800

    初始化

---
+++ src/main/java/top/thexiaola/dreamhwhub/DreamHwhubApplication.java
@@ -0,0 +1,13 @@
+package top.thexiaola.dreamhwhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamHwhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamHwhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/thexiaola/dreamhwhub/DreamHwhubApplicationTests.java
@@ -0,0 +1,13 @@
+package top.thexiaola.dreamhwhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamHwhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
