39e346969deb219e92164bd61e1168d6c8139758
TheXiaoLa <167291828@qq.com>
2026-03-21 22:08:20 +0800

    存档

--- src/main/java/top/thexiaola/dreamhwhub/module/login/controller/ModifyUserController.java
+++ src/main/java/top/thexiaola/dreamhwhub/module/login/controller/ModifyUserController.java
@@ -1,10 +1,27 @@
 package top.thexiaola.dreamhwhub.module.login.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class ModifyUserController {
-    // TODO: 用户修改自己的信息
+    @PostMapping("/modify/info")
+    public String modifyUserInfo() {
+        // TODO: 需要允许用户修改自己的 user_no、username、id_name、phone
+        return null;
+    }
+
+    @PostMapping("/modify/email")
+    public String modifyUserEmail() {
+        // TODO: 需要允许用户修改自己的 email
+        return null;
+    }
+
+    @PostMapping("/modify/password")
+    public String modifyUserPassword() {
+        // TODO: 需要允许用户修改自己的密码
+        return null;
+    }
 }
